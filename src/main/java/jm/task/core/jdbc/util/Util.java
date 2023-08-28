47580442f9f7695df0ffbb312b9e69f9d33f7adc
Egor Pushka <pushkartegor@gmail.com>
2023-08-28 12:45:09 +0300

    First Commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_USER_PASSWORD = "root";
+    private static final String DB_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Connection connection;
+        Class.forName(DB_DRIVER_NAME); //динамическая загрузка драйвера, лучше чем статическая
+        connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_USER_PASSWORD);
+        System.out.println("BD :: Start Connection");
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        connection.close();
+        System.out.println("BD :: Close Connection");
+    }
+
+}
---
+++ src/test/resources/test.sql
@@ -0,0 +1,5 @@
+insert into users (name, lastName, age) values('Igor', 'Petrov', '42')
+#select * from users;
+#select name, age from users where id = 2;
+#update users set name='Max', age=66 where id=1;
+#delete from users where id=4;
